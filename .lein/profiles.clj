4037f1ab04fc04380a800e7fa4748124151bc013
Nguyễn Tuấn Anh <ubolonton@gmail.com>
2013-10-11 17:34:33 +0700

    add lein profile

---
+++ .lein/profiles.clj
@@ -0,0 +1,8 @@
+{:user {:plugins [[lein-difftest "1.3.7"]
+                  [lein-marginalia "0.7.0"]
+                  [lein-pprint "1.1.1"]
+                  [lein-swank "1.4.3"]
+                  [lein-noir "1.2.1"]
+                  [noir/lein-template "1.3.0"]
+                  [lein-cljsbuild "0.3.3"]]
+        :dependencies [[com.cemerick/pomegranate "0.0.13"]]}}
