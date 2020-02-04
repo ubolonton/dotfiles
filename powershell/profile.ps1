60dec77a435f7c2b0a73de2dd9a36d9e8a075f77
Tuấn-Anh Nguyễn <ubolonton@gmail.com>
2020-02-04 16:44:11 +0700

    Add PowerShell init script

---
+++ powershell/profile.ps1
@@ -0,0 +1,12 @@
+# sudo New-Item -ItemType SymbolicLink -path $HOME\Documents\WindowsPowerShell\ -name profile.ps1 -value $HOME\dotfiles\powershell\profile.ps1
+
+echo "--------------------------------------------------------------------------------"
+echo "Running profile.ps1"
+
+# TODO: Shouldn't there be a way to modify this structurally? This is not unix, so do better.
+$env:Path = "$HOME\scoop\shims" + $env:Path
+$env:Path = "$HOME\scoop\apps\msys2\current\mingw64\bin;" + $env:Path
+$env:Path = "$HOME\.cask\bin;" + $env:Path
+
+echo "Done running profile.ps1"
+echo "--------------------------------------------------------------------------------"
