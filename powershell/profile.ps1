8b731c02751bac55ca841de400838d128ec6f6e9
Tuấn-Anh Nguyễn <ubolonton@gmail.com>
2021-09-05 12:57:39 +0700

    pwsh: Improve $env:Path

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -4,9 +4,19 @@ echo "--------------------------------------------------------------------------
 echo "Running profile.ps1"
 
 # TODO: Shouldn't there be a way to modify this structurally? This is not unix, so do better.
-$env:Path = "$HOME\scoop\shims" + $env:Path
-$env:Path = "$(scoop prefix msys2)\mingw64\bin;" + $env:Path
+
+$msys2_prefix = "$(scoop prefix msys2)"
+$env:Path += ";$msys2_prefix\usr\local\bin"
+$env:Path += ";$msys2_prefix\usr\bin"
+$env:Path = "$msys2_prefix\mingw64\bin;" + $env:Path
+
+$env:Path = "$HOME\scoop\shims;" + $env:Path
+
 $env:Path = "$HOME\.cask\bin;" + $env:Path
+
+$env:Path = "$HOME\bin;" + $env:Path
+
+$env:Path += ";C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Tools\MSVC\14.22.27905\bin\HostX86\x86"
 
 echo "Done running profile.ps1"
 echo "--------------------------------------------------------------------------------"
