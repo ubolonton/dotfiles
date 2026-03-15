74abac779297e6d08e5a2d5f2c30a64732cd76dc
Tuấn-Anh Nguyễn <ubolonton@gmail.com>
2026-03-15 21:16:46 +0700

    feat(pwsh): Add basic key bindings

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -18,17 +18,28 @@ $env:Path = "$HOME\scoop\shims;" + $env:Path
 
 (&mise activate pwsh) | Out-String | Invoke-Expression
 
-#
 Invoke-Expression (&starship init powershell)
 
-# https://github.com/dahlbyk/posh-sshell
-# https://itsallinthecode.com/powershell-using-git-with-ssh-keys-on-windows-10/
-try {
-    Import-Module posh-sshell
-    Start-SshAgent
-} catch {
-    echo "Fail to set up ssh agent integration"
-}
+# Get-PSReadLineKeyHandler
+# https://learn.microsoft.com/en-us/powershell/module/psreadline/set-psreadlinekeyhandler?view=powershell-7.5
+Set-PSReadLineKeyHandler -Chord Ctrl+a -Function BeginningOfLine # was SelectAll
+Set-PSReadLineKeyHandler -Chord Ctrl+e -Function EndOfline
+Set-PSReadLineKeyHandler -Chord Ctrl+k -Function ForwardDeleteInput
+Set-PSReadLineKeyHandler -Chord Ctrl+Shift+k -Function RevertLine
+Set-PSReadLineKeyHandler -Chord Alt+Spacebar -Function YankLastArg
+# XXX: Can't do this.
+#
+# Set-PSReadLineKeyHandler: Invalid sequence 'Ctrl+x,@,s,z'.
+# Set-PSReadLineKeyHandler -Chord 'Ctrl+x,@,s,z' -Function Undo
+
+# # https://github.com/dahlbyk/posh-sshell
+# # https://itsallinthecode.com/powershell-using-git-with-ssh-keys-on-windows-10/
+# try {
+#     Import-Module posh-sshell
+#     Start-SshAgent
+# } catch {
+#     echo "Fail to set up ssh agent integration"
+# }
 
 echo "Done running profile.ps1"
 echo "--------------------------------------------------------------------------------"
